e731061148e95d523282841e29f657ef389f5bf9
z631200 <z631200@gmail.com>
2025-11-06 18:37:17 +0800

    finish manager_v1

--- backstage/views/manager_new.py
+++ backstage/views/manager_new.py
@@ -12,18 +12,19 @@ from flask import current_app
 UPLOAD_FOLDER = 'static/product'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
-manager = Blueprint('manager', __name__, template_folder='../templates')
+manager_new = Blueprint('manager', __name__, template_folder='../templates')
 
 def config():
     current_app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     config = current_app.config['UPLOAD_FOLDER'] 
     return config
 
-@manager.route('/', methods=['GET', 'POST'])
+@manager_new.route('/', methods=['GET', 'POST'])
 def home():
     return redirect(url_for('manager.productManager'))
 
-@manager.route('/personManager', methods=['GET', 'POST'])
+# ================== Person Management =====================
+@manager_new.route('/personManager', methods=['GET', 'POST'])
 def personManager():
     if 'delete' in request.values:
         pid = request.values.get('delete')
@@ -60,7 +61,7 @@ def person():
         person_data.append(person)
     return person_data
 
-@manager.route('/add_person', methods=['GET', 'POST'])
+@manager_new.route('/add_person', methods=['GET', 'POST'])
 def add_person():
     if request.method == 'POST':
 
@@ -91,7 +92,7 @@ def add_person():
     
     return render_template('personManager.html')
 
-@manager.route('/edit_person', methods=['GET', 'POST'])
+@manager_new.route('/edit_person', methods=['GET', 'POST'])
 def edit_person():
     if request.method == 'POST':
         Person.update_person(
@@ -125,11 +126,12 @@ def show_person_info():
     }
     return person
 
-@manager.route('/logisticsManager', methods=['GET', 'POST'])
+# ================== Logistics Management =====================
+@manager_new.route('/logisticsManager', methods=['GET', 'POST'])
 def logisticsManager():
     if 'delete' in request.values:
         logistics_name = request.values.get('delete')
-        Person.delete_person(logistics_name)
+        Logistics.delete_logistics(logistics_name)
 
     #     if 'delete' in request.values:
     #         pid = request.values.get('delete')
@@ -143,118 +145,329 @@ def logisticsManager():
 
     elif 'edit' in request.values:
         logistics_name = request.values.get('edit')
-        return redirect(url_for('manager.edit_person', logistics_name=logistics_name))
-    
+        return redirect(url_for('manager.edit_logistics', logistics_name=logistics_name))
 
-    person_data = person()
-    return render_template('personManager.html', person_data = person_data, user=current_user.name)
+    Logistics_data = logistics()
+    return render_template('personManager.html', person_data = Logistics, user=current_user.name)
 
-def person():
-    person_row = Person.get_all_person()
-    person_data = []
-    for i in person_row:
-        person = {
-            '使用者編號': i[0],
-            '使用者名稱': i[1],
-            '使用者角色': i[2],
-            '使用者信箱': i[3]
+def logistics():
+    logistics_row = Logistics.get_all_logistics()
+    logistics_data = []
+    for i in logistics_row:
+        logistics = {
+            '後勤名稱': i[0],
+            '工作內容': i[1]
         }
-        person_data.append(person)
-    return person_data
+        logistics_data.append(logistics)
+    return logistics_data
 
-@manager.route('/add_person', methods=['GET', 'POST'])
-def add_person():
+@manager_new.route('/add_logistics', methods=['GET', 'POST'])
+def add_logistics():
     if request.method == 'POST':
 
         # note: not completed
-        pname = request.values.get('pname')
-        gender = request.values.get('gender')
-        department = request.values.get('department')
-        grade = request.values.get('grade')
+        logistics_name = request.values.get('logistics_name')
+        logistics_description = request.values.get('description')
 
         # validation, can be extended
-        if pname is None:
+        if logistics_name is None:
             flash('所有欄位都是必填的，請確認輸入內容。')
-            return redirect(url_for('manager.personManager'))
-        if len(pname) < 1:
-            flash('使用者名稱不可為空。')
-            return redirect(url_for('manager.personManager'))
+            return redirect(url_for('manager.logisticsManager'))
+        if len(logistics_name) < 1:
+            flash('後勤名稱不可為空。')
+            return redirect(url_for('manager.logisticsManager'))
 
-        Person.add_person(
+        Logistics.add_logistics(
             {
-            'pname' : pname,
-            'gender' : gender,
-            'department' : department,
-            'grade':grade
+            'logistics_name' : logistics_name,
+            'logistics_description' : logistics_description
             }
         )
 
-        return redirect(url_for('manager.personManager'))
+        return redirect(url_for('manager.logisticsManager'))
     
-    return render_template('personManager.html')
+    return render_template('logisticsManager.html')
 
-@manager.route('/edit_person', methods=['GET', 'POST'])
-def edit_person():
+@manager_new.route('/edit_logistics', methods=['GET', 'POST'])
+def edit_logistics():
     if request.method == 'POST':
-        Person.update_person(
+        Logistics.update_logistics(
             {
-            'pname' : request.values.get('pname'),
-            'gender' : request.values.get('gender'),
-            'department' : request.values.get('department'),
-            'grade' : request.values.get('grade'),
-            'pid' : request.values.get('pid')
+            'logistics_name' : request.values.get('logistics_name'),
+            'logistics_description' : request.values.get('description'),
             }
         )
-        return redirect(url_for('manager.personManager'))
+        return redirect(url_for('manager.logisticsManager'))
     else:
-        person = show_person_info()
-        return render_template('edit_person.html', data=person)
+        logistics = show_logistics_info()
+        return render_template('edit_logistics.html', data=logistics)
+
+def show_logistics_info():
+    logistics_name = request.args['logistics_name']
+    data = Logistics.get_logistics(logistics_name)
+    logistics_description = data[1]
     
-def show_person_info():
-    pid = request.args['pid']
-    data = Person.get_person(pid)
-    pname = data[1]
-    gender = data[2]
-    department = data[3]
-    grade = data[4]
-    
-    person = {
-        '使用者編號': pid,
-        '使用者名稱': pname,
-        '使用者性別': gender,
-        '使用者系別': department,
-        '使用者年級': grade
+    logistics = {
+        '後勤名稱': logistics_name,
+        '工作內容': logistics_description
     }
-    return person
+    return logistics
 
+# ================== Equipment Management =====================
+@manager_new.route('/equipmentManager', methods=['GET', 'POST'])
+def equipmentManager():
+    if 'delete' in request.values:
+        equipment_id = request.values.get('delete')
+        Equipment.delete_equipment(equipment_id)
 
-# @manager.route('/orderManager', methods=['GET', 'POST'])
-# @login_required
-# def orderManager():
-#     if request.method == 'POST':
-#         pass
-#     else:
-#         order_row = Order_List.get_order()
-#         order_data = []
-#         for i in order_row:
-#             order = {
-#                 '訂單編號': i[0],
-#                 '訂購人': i[1],
-#                 '訂單總價': i[2],
-#                 '訂單時間': i[3]
-#             }
-#             order_data.append(order)
-            
-#         orderdetail_row = Order_List.get_orderdetail()
-#         order_detail = []
+    elif 'edit' in request.values:
+        equipment_id = request.values.get('edit')
+        return redirect(url_for('manager.edit_equipment', equipment_id=equipment_id))
 
-#         for j in orderdetail_row:
-#             orderdetail = {
-#                 '訂單編號': j[0],
-#                 '商品名稱': j[1],
-#                 '商品單價': j[2],
-#                 '訂購數量': j[3]
-#             }
-#             order_detail.append(orderdetail)
+    equipment_data = equipment()
+    return render_template('equipmentManager.html', equipment_data = equipment_data, user=current_user.name)
 
-#     return render_template('orderManager.html', orderData = order_data, orderDetail = order_detail, user=current_user.name)
+def equipment():
+    equipment_row = Equipment.get_all_equipment()
+    equipment_data = []
+    for i in equipment_row:
+        equipment = {
+            '設備編號': i[0],
+            '設備名稱': i[1],
+            '設備狀態': i[2],
+            '設備描述': i[3]
+        }
+        equipment_data.append(equipment)
+    return equipment_data
+
+@manager_new.route('/add_equipment', methods=['GET', 'POST'])
+def add_equipment():
+    if request.method == 'POST':
+
+        # note: not completed
+        equipment_name = request.values.get('equipment_name')
+        equipment_status = request.values.get('equipment_status')
+        equipment_description = request.values.get('description')
+
+        # validation, can be extended
+        if equipment_name is None:
+            flash('所有欄位都是必填的，請確認輸入內容。')
+            return redirect(url_for('manager.equipmentManager'))
+        if len(equipment_name) < 1:
+            flash('設備名稱不可為空。')
+            return redirect(url_for('manager.equipmentManager'))
+
+        Equipment.add_equipment(
+            {
+            'equipment_name' : equipment_name,
+            'equipment_status' : equipment_status,
+            'equipment_description' : equipment_description
+            }
+        )
+
+        return redirect(url_for('manager.equipmentManager'))
+    
+    return render_template('equipmentManager.html')
+
+@manager_new.route('/edit_equipment', methods=['GET', 'POST'])
+def edit_equipment():
+    if request.method == 'POST':
+        Equipment.update_equipment(
+            {
+            'equipment_name' : request.values.get('equipment_name'),
+            'equipment_status' : request.values.get('equipment_status'),
+            'equipment_description' : request.values.get('description'),
+            'equipment_id' : request.values.get('equipment_id')
+            }
+        )
+        return redirect(url_for('manager.equipmentManager'))
+    else:
+        equipment = show_equipment_info()
+        return render_template('edit_equipment.html', data=equipment)
+
+def show_equipment_info():
+    equipment_id = request.args['equipment_id']
+    data = Equipment.get_equipment(equipment_id)
+    equipment_name = data[1]
+    equipment_status = data[2]
+    equipment_description = data[3]
+    
+    equipment = {
+        '設備編號': equipment_id,
+        '設備名稱': equipment_name,
+        '設備狀態': equipment_status,
+        '設備描述': equipment_description
+    }
+    return equipment
+
+# ================== Activity Management =====================
+@manager_new.route('/activityManager', methods=['GET', 'POST'])
+def acitvityManager():
+    if 'delete' in request.values:
+        activity_id = request.values.get('delete')
+        Activity.delete_activity(activity_id)
+    elif 'edit' in request.values:
+        activity_id = request.values.get('edit')
+        return redirect(url_for('manager.edit_activity', activity_id=activity_id))
+
+    activity_data = activity()
+    return render_template('activityManager.html')
+
+def activity():
+    activity_row = Activity.get_all_activity()
+    activity_data = []
+    for i in activity_row:
+        activity = {
+            '活動編號': i[0],
+            '活動名稱': i[1],
+            '活動日期': i[2],
+            '活動描述': i[3]
+        }
+        activity_data.append(activity)
+    return activity_data
+
+@manager_new.route('/add_activity', methods=['GET', 'POST'])
+def add_activity():
+    if request.method == 'POST':
+
+        # note: not completed
+        activity_name = request.values.get('activity_name')
+        activity_date = request.values.get('activity_date')
+        activity_description = request.values.get('description')
+
+        # validation, can be extended
+        if activity_name is None:
+            flash('所有欄位都是必填的，請確認輸入內容。')
+            return redirect(url_for('manager.activityManager'))
+        if len(activity_name) < 1:
+            flash('活動名稱不可為空。')
+            return redirect(url_for('manager.activityManager'))
+
+        Activity.add_activity(
+            {
+            'activity_name' : activity_name,
+            'activity_date' : activity_date,
+            'activity_description' : activity_description
+            }
+        )
+
+        return redirect(url_for('manager.activityManager'))
+    
+    return render_template('activityManager.html')
+
+@manager_new.route('/edit_activity', methods=['GET', 'POST'])
+def edit_activity():
+    if request.method == 'POST':
+        Activity.update_activity(
+            {
+            'activity_name' : request.values.get('activity_name'),
+            'activity_date' : request.values.get('activity_date'),
+            'activity_description' : request.values.get('description'),
+            'activity_id' : request.values.get('activity_id')
+            }
+        )
+        return redirect(url_for('manager.activityManager'))
+    else:
+        activity = show_activity_info()
+        return render_template('edit_activity.html', data=activity)
+
+def show_activity_info():
+    activity_id = request.args['activity_id']
+    data = Activity.get_activity(activity_id)
+    activity_name = data[1]
+    activity_date = data[2]
+    activity_description = data[3]
+    
+    activity = {
+        '活動編號': activity_id,
+        '活動名稱': activity_name,
+        '活動日期': activity_date,
+        '活動描述': activity_description
+    }
+    return activity
+
+# =================== Program Management =====================
+@manager_new.route('/programManager', methods=['GET', 'POST'])
+def programManager():
+    if 'delete' in request.values:
+        program_id = request.values.get('delete')
+        Program.delete_program(program_id)
+    elif 'edit' in request.values:
+        program_id = request.values.get('edit')
+        return redirect(url_for('manager.edit_program', program_id=program_id))
+   
+    program_data = program()
+    return render_template('programManager.html')
+
+def program():
+    program_row = Program.get_all_program()
+    program_data = []
+    for i in program_row:
+        program = {
+            '節目編號': i[0],
+            '節目名稱': i[1],
+            '節目主持人': i[2],
+            '節目時間': i[3]
+        }
+        program_data.append(program)
+    return program_data
+
+@manager_new.route('/add_program', methods=['GET', 'POST'])
+def add_program():
+    if request.method == 'POST':
+
+        # note: not completed
+        program_name = request.values.get('program_name')
+        program_host = request.values.get('program_host')
+        program_time = request.values.get('program_time')
+
+        # validation, can be extended
+        if program_name is None:
+            flash('所有欄位都是必填的，請確認輸入內容。')
+            return redirect(url_for('manager.programManager'))
+        if len(program_name) < 1:
+            flash('節目名稱不可為空。')
+            return redirect(url_for('manager.programManager'))
+
+        Program.add_program(
+            {
+            'program_name' : program_name,
+            'program_host' : program_host,
+            'program_time' : program_time
+            }
+        )
+
+        return redirect(url_for('manager.programManager'))
+    
+    return render_template('programManager.html')
+
+@manager_new.route('/edit_program', methods=['GET', 'POST'])
+def edit_program():
+    if request.method == 'POST':
+        Program.update_program(
+            {
+            'program_name' : request.values.get('program_name'),
+            'program_host' : request.values.get('program_host'),
+            'program_time' : request.values.get('program_time'),
+            'program_id' : request.values.get('program_id')
+            }
+        )
+        return redirect(url_for('manager.programManager'))
+    else:
+        program = show_program_info()
+        return render_template('edit_program.html', data=program)
+
+def show_program_info():
+    program_id = request.args['program_id']
+    data = Program.get_program(program_id)
+    program_name = data[1]
+    program_host = data[2]
+    program_time = data[3]
+    
+    program = {
+        '節目編號': program_id,
+        '節目名稱': program_name,
+        '節目主持人': program_host,
+        '節目時間': program_time
+    }
+    return program
